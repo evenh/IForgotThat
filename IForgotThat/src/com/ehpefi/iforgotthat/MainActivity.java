bc0700230d59f5c87a17e93c0f8138a9467e7e67
Even Holthe <even.holthe@me.com>
2014-03-20 18:11:40 -0700

    Now showing the different lists happens in a separate method

--- IForgotThat/src/com/ehpefi/iforgotthat/MainActivity.java
+++ IForgotThat/src/com/ehpefi/iforgotthat/MainActivity.java
@@ -19,8 +19,13 @@ import android.widget.ViewSwitcher;
  * @since 1.0
  */
 public class MainActivity extends Activity {
+	// UI Elements
 	ListView listView;
 	EditText listNameInput;
+
+	// Database related
+	ListHelper listHelper = new ListHelper(this);
+	ListElementHelper listElementHelper = new ListElementHelper(this);
 
 	// Used for logging
 	private static final String TAG = "MainActivity";
@@ -30,42 +35,50 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// Find our UI elements
 		listView = (ListView) findViewById(R.id.mainView);
 		listNameInput = (EditText) findViewById(R.id.editable_add_list_input);
-		
+
+		showLists();
+	}
+
+	private void showLists() {
+		// If the database doesn't contain any lists
+		if (listHelper.numberOfLists() == 0) {
+
+			Log.d(TAG, "No lists in the database");
+
+			return;
+		}
+
 		// for development only. the array will get its data from the database in prod
-		String[] values = new String[] { "Important", 
-														"Weekend reminders",
-														"Today", "Before I go to bed", 
-														"Before next week" };
+		String[] values = new String[] { "Important", "Weekend reminders", "Today", "Before I go to bed",
+				"Before next week" };
 
-
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+				android.R.id.text1, values);
 
 		// assign adapter to listView
 		listView.setAdapter(adapter);
-		
+
 		// set clicklistener
-		listView.setOnItemClickListener(new OnItemClickListener(){
+		listView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				
+
 				// get clicked item value
-				String itemValue = (String) listView .getItemAtPosition(position);
-				 
-				Intent intent = new Intent(getApplicationContext(),	ListWithElementsActivity.class);
-				intent.putExtra("name", itemValue);//add the item name to the intent
+				String itemValue = (String) listView.getItemAtPosition(position);
+
+				Intent intent = new Intent(getApplicationContext(), ListWithElementsActivity.class);
+				intent.putExtra("name", itemValue);// add the item name to the intent
 
 				startActivity(intent);
 
-				//transition
+				// transition
 				overridePendingTransition(R.anim.right_in, R.anim.left_out);
 			}
 		});// end of clicklistener
-
-	}// end of onCreate
-
+	}
 
 	/**
 	 * Requests the change from TextView to EditText
@@ -78,7 +91,7 @@ public class MainActivity extends Activity {
 		switcher.showNext();
 	}
 
-	public void addNewListTextEntered(View view){
+	public void addNewListTextEntered(View view) {
 		// TODO: Make this a method not used just for debugging purposes
 		Log.d(TAG, "The list name that was entered was: " + listNameInput.getText());
 	}
