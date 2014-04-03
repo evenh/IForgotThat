dd5443f4633650b8a5fbf688d1a0d90a961a92ad
Per Erik <pererikfinstad@gmail.com>
2014-04-02 20:31:09 -0700

    Making camera appear on screen. not working

--- IForgotThat/src/com/ehpefi/iforgotthat/CameraActivity.java
+++ IForgotThat/src/com/ehpefi/iforgotthat/CameraActivity.java
@@ -1,22 +1,48 @@
 package com.ehpefi.iforgotthat;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
+
 
 public class CameraActivity extends Activity {
 
+	private static final String TAG = "CameraActivty";
+	private Camera mCamera;
+	private CameraPreview mPreview;
+
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
+		
+		
+
+		// Create an instance of Camera
+		mCamera = getCameraInstance();
+
+		// Create preview view and set it as the content of our activity.
+		mPreview = new CameraPreview(this, mCamera);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(mPreview);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.camera, menu);
-		return true;
-	}
+	// get an instance of the camera
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+			Log.d(TAG, "Camera is not available or doesn't exist " + e.getMessage());
 
-}
+		}
+		return c; // returns null if camera is unavailable
+	}
+	
+
+
+}//end of class
