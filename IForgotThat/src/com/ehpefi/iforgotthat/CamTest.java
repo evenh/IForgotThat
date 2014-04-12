49f8124a9f44edfe8507e5becbd44dd45af0fd72
Per Erik <pererikfinstad@gmail.com>
2014-04-11 22:03:19 -0700

    Created a new camera activity with square previews

---
+++ IForgotThat/src/com/ehpefi/iforgotthat/CamTest.java
@@ -0,0 +1,334 @@
+package com.ehpefi.iforgotthat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.PictureCallback;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class CamTest extends Activity implements SurfaceHolder.Callback {
+	ListHelper listHelper = new ListHelper(this);
+
+	private static final String TAG = "CAMERA";
+	private Camera camera;
+	private SurfaceView surfaceView;
+	private SurfaceHolder surfaceHolder;
+	boolean isPreviewActive = false;
+	private int listID;;
+	private TextView title;
+	private ImageButton captureButton;
+	private ImageButton flashButton;
+	private String listTitle;
+
+	private PictureCallback mPicture;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cam_test);
+		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		    
+	      getWindow().setFormat(PixelFormat.UNKNOWN);
+	      surfaceView = (SurfaceView)findViewById(R.id.camerapreview);
+	      surfaceHolder = surfaceView.getHolder();
+	      surfaceHolder.addCallback(this);
+
+		camera = getCameraInstance();
+
+		// Check for incoming data and set list title
+		Bundle bundle = getIntent().getExtras();
+		if (bundle != null) {
+			// Check for list id
+			if (bundle.getInt("listID") > 0) {
+				listID = bundle.getInt("listID");
+				Log.d(TAG, "List id recieved: " + listID);
+
+				ListObject list = listHelper.getList(listID);
+				listTitle = list.getTitle();
+				Log.d(TAG, "List title resolved: " + listTitle);
+
+				// Get TextView for the list title and set its name
+				title = (TextView) findViewById(R.id.listName);
+				title.setText(listTitle);
+			}
+		}
+
+		// Get the UI elements
+		captureButton = (ImageButton) findViewById(R.id.button_capture);
+		flashButton = (ImageButton) findViewById(R.id.button_flash);
+
+		mPicture = new PictureCallback() {
+			@Override
+			public void onPictureTaken(byte[] data, Camera camera) {
+				Log.d(TAG, "PictureCallback method is called");
+				// Compress the data
+				data = compressImage(data);
+
+				// Call the NewReminderActivity
+				createNewReminder(data);
+			}
+		};
+
+		// capture image button listener
+		ImageButton captureButton = (ImageButton) findViewById(R.id.button_capture);
+		captureButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// get an image from the camera
+				Log.d(TAG, "Photo in the taking!");
+
+				// Hack for the Google Nexus S
+				if (Build.MODEL.equals("Nexus S")
+						&& camera.getParameters().getFlashMode().equals(Parameters.FLASH_MODE_ON)) {
+					Log.i(TAG, "Google Nexus S detected, applying flash hack!");
+					Camera.Parameters tempParam = camera.getParameters();
+					tempParam.setFlashMode(Parameters.FLASH_MODE_TORCH);
+					camera.setParameters(tempParam);
+					try {
+						Thread.sleep(300);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					camera.takePicture(null, null, mPicture);
+					Log.d(TAG, "Picture captured");
+
+					try {
+						Thread.sleep(500);
+						tempParam.setFlashMode(Parameters.FLASH_MODE_ON);
+						camera.setParameters(tempParam);
+					} catch (InterruptedException ie) {
+						ie.printStackTrace();
+					} catch (Exception e) {
+						Log.e(TAG, "Exception while setting the parameters", e);
+					}
+
+				} else {
+					camera.takePicture(null, null, mPicture);
+				}
+			}
+		});
+
+	}
+
+	
+	/**
+	 * Fires off the new intent to NewReminderActivity
+	 * 
+	 * @param A
+	 *              compressed image as a byte array
+	 * @since 1.0
+	 */
+	private void createNewReminder(byte[] image) {
+		Intent intent = new Intent(this, NewReminderActivity.class);
+		// Put all the necessary extras in
+		intent.putExtra("image", image);
+
+		// Check for incoming data from the previous activity
+		Bundle bundle = getIntent().getExtras();
+		if (bundle != null && bundle.getInt("listID") != 0) {
+			intent.putExtra("listID", bundle.getInt("listID"));
+		} else {
+			intent.putExtra("listID", 0);
+		}
+
+		startActivity(intent);
+
+		// Transition smoothly
+		overridePendingTransition(R.anim.right_in, R.anim.left_out);
+	}
+
+
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		if (isPreviewActive) {
+			camera.stopPreview();
+			isPreviewActive = false;
+		}
+
+		if (camera != null) {
+			try {
+				camera.setPreviewDisplay(surfaceHolder);
+				camera.startPreview();
+				isPreviewActive = true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		// FIX for rotation/strecht issue on NEXUS S
+		if (holder.getSurface() == null) {
+			// preview surface does not exist
+			return;
+		}
+
+		// stop preview before making changes
+		try {
+			camera.stopPreview();
+		} catch (Exception e) {
+			// ignore: tried to stop a non-existent preview
+		}
+
+		// make any resize, rotate or reformatting changes here
+		if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+			camera.setDisplayOrientation(90);
+		} else {
+			camera.setDisplayOrientation(0);
+		}
+		// start preview with new settings
+		try {
+			camera.setPreviewDisplay(holder);
+			camera.startPreview();
+
+		} catch (Exception e) {
+			Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+		}
+	}
+
+
+	public static Camera getCameraInstance() {
+
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+			// find supported image sizes
+			try {
+				Parameters params = c.getParameters();
+
+				List<Camera.Size> cameraSizes = params.getSupportedPictureSizes();
+				// selects the smallest resolution (last item in array)
+				Camera.Size selectedPictureSize = cameraSizes.get(cameraSizes.size() - 1);
+				Log.i(TAG, "Camera resolution selected: " + selectedPictureSize.width + "x"
+						+ selectedPictureSize.height);
+				params.setPictureSize(selectedPictureSize.width, selectedPictureSize.height);
+
+				List<Camera.Size> previewSizes = params.getSupportedPreviewSizes();
+				Camera.Size selectedPreviewSize = previewSizes.get(previewSizes.size() - 1);
+				Log.i(TAG, "Preview resolution selected: " + selectedPreviewSize.width + "x"
+						+ selectedPreviewSize.height);
+				// params.setPreviewSize(selectedPreviewSize.width, selectedPreviewSize.height);
+
+				c.setParameters(params);
+			} catch (Exception e) {
+				Log.d(TAG, "Failed to set parameters: " + e);
+			}
+
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+			Log.e(TAG, "Camera is not available or doesn't exist" + e);
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	
+
+	/**
+	 * Compresses a raw image from the camera to JPEG with 70% quality
+	 * 
+	 * @param A
+	 *              raw image in a byte array
+	 * @return A byte array containing the compressed image
+	 * @since 1.0
+	 */
+	private byte[] compressImage(byte[] input) {
+		Log.d(TAG, "Size of image before compression: " + input.length / 1024 + " kB");
+
+		Bitmap original = BitmapFactory.decodeByteArray(input, 0, input.length);
+		ByteArrayOutputStream blob = new ByteArrayOutputStream();
+
+		Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+		if (Build.MODEL.equals("Nexus S") && display.getRotation() == Surface.ROTATION_0) {
+			Log.i(TAG, "Nexus S detected, applying rotation hack!");
+			Matrix matrix = new Matrix();
+			matrix.postRotate(90);
+			Bitmap rotated = Bitmap.createBitmap(original, 0, 0, original.getWidth(), original.getHeight(),
+					matrix, true);
+			rotated.compress(Bitmap.CompressFormat.JPEG, 70, blob);
+		} else {
+			original.compress(Bitmap.CompressFormat.JPEG, 70, blob);
+		}
+
+		byte[] compressed = blob.toByteArray();
+		Log.d(TAG, "Size of image after compression: " + compressed.length / 1024 + " kB");
+
+		return compressed;
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+
+		camera.stopPreview();
+		camera.release();
+		camera = null;
+		isPreviewActive = false;
+	}
+
+
+
+	/**
+	 * Method for turning camera flash on/off. Default is off
+	 * 
+	 * @param The view
+	 * @since 1.0
+	 */
+	public void flashOnOff(View v) {
+		Log.d(TAG, "Flash button pressed");
+		Camera.Parameters param = camera.getParameters();
+		List<String> flashModes = param.getSupportedFlashModes();
+
+		if (flashModes != null) {
+			String currentFlashMode = param.getFlashMode();
+
+			if (currentFlashMode.equals(Parameters.FLASH_MODE_OFF)) {
+				currentFlashMode = Parameters.FLASH_MODE_ON;
+			} else {
+				currentFlashMode = Parameters.FLASH_MODE_OFF;
+			}
+
+			param.setFlashMode(currentFlashMode);
+			camera.setParameters(param);
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		Intent intent = new Intent(this, ListWithElementsActivity.class);
+		intent.putExtra("listID", getIntent().getExtras().getInt("listID"));
+		startActivityForResult(intent, 0);
+		// Transition animation
+		overridePendingTransition(R.anim.left_in, R.anim.right_out);
+	}
+
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}// end of class CamTest
+
