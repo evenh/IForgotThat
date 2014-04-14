b0fc15fa0bce0609392639736e8a48fdff33e50d
Even Holthe <even.holthe@me.com>
2014-04-14 16:38:39 -0700

    Removes the ability to specify a date back in time

--- IForgotThat/src/com/ehpefi/iforgotthat/NewReminderActivity.java
+++ IForgotThat/src/com/ehpefi/iforgotthat/NewReminderActivity.java
@@ -161,12 +161,14 @@ public class NewReminderActivity extends Activity {
 		final DatePicker datePicker = (DatePicker) dateTimeView.findViewById(R.id.datePicker);
 		final TimePicker timePicker = (TimePicker) dateTimeView.findViewById(R.id.timePicker);
 
+		datePicker.setMinDate(System.currentTimeMillis() - 1000);
+
 		// If the user has previously selected a date+time
 		if (reminder != null) {
 			Calendar cal = Calendar.getInstance();
 			cal.setTime(reminder);
 
-			datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) - 1, cal.get(Calendar.DAY_OF_MONTH), null);
+			datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
 			timePicker.setCurrentHour(cal.get(Calendar.HOUR));
 			timePicker.setCurrentMinute(cal.get(Calendar.MINUTE));
 		}
