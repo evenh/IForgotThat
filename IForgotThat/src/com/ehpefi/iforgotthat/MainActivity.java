3da24b48a23689dfb58a74639a403a07a6a91a2e
Per Erik <pererikfinstad@gmail.com>
2014-04-24 20:02:38 -0700

    Close application when onBackPressed on Main Activty

--- IForgotThat/src/com/ehpefi/iforgotthat/MainActivity.java
+++ IForgotThat/src/com/ehpefi/iforgotthat/MainActivity.java
@@ -333,4 +333,11 @@ public class MainActivity extends Activity {
 		super.onDestroy();
 	}
 
+	public void onBackPressed() {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
+	}
+
 }
