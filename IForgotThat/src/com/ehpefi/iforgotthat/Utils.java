7fa57f4167c832dfac0c6468f9215cbbf531c97e
Even Holthe <even.holthe@me.com>
2014-05-21 22:57:06 -0700

    Fixes the isLocationEnabled() check to work on API level <19

--- IForgotThat/src/com/ehpefi/iforgotthat/Utils.java
+++ IForgotThat/src/com/ehpefi/iforgotthat/Utils.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
@@ -26,20 +27,32 @@ public class Utils {
 	 * @since 1.0.0
 	 */
 	public static boolean isLocationEnabled(final Context context) {
-		int locationProviders = Settings.Secure.LOCATION_MODE_OFF;
+		if (android.os.Build.VERSION.SDK_INT >= 19) {
+			int locationProviders = Settings.Secure.LOCATION_MODE_OFF;
 
-		// Fetch the location mode
-		try {
-			locationProviders = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
-		} catch (SettingNotFoundException e) {
-			Log.e(TAG, "LOCATION_MODE not found!");
+			// Fetch the location mode
+			try {
+				locationProviders = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+			} catch (SettingNotFoundException e) {
+				Log.e(TAG, "LOCATION_MODE not found!", e);
+			}
+
+			// If we have location enabled, return true
+			if (locationProviders != Settings.Secure.LOCATION_MODE_OFF) {
+				return true;
+			}
+		} else {
+			// Below API level 19
+			LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+			try {
+				if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER) || lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+					// We have location providers
+					return true;
+				}
+			} catch (Exception ex) {
+				Log.d(TAG, "Got exception while trying to fetch location providers in legacy mode", ex);
+			}
 		}
-
-		// If we have location enabled, return true
-		if (locationProviders != Settings.Secure.LOCATION_MODE_OFF) {
-			return true;
-		}
-
 		// Show a dialog that location has to be enabled
 		Dialog dialog = new AlertDialog.Builder(context).setTitle(R.string.no_loc_providers_enabled_title).setMessage(R.string.no_loc_providers_enabled_message)
 				.setPositiveButton(R.string.no_loc_providers_enabled_ok, new DialogInterface.OnClickListener() {
